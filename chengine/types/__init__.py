72535975251635ad1b57413a2cc22c9a7e33617f
harrywiner <winer.harry@gmail.com>
2025-07-20 12:39:48 +0100

    reorganised types

---
+++ chengine/types/__init__.py
@@ -0,0 +1,7 @@
+from .types import Eval, Board, Player
+
+__all__ = [
+    "Eval",
+    "Board",
+    "Player"
+]
---
+++ chengine/types/types.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+from typing import List
+from abc import ABC, abstractmethod
+
+Board = List[List[str]]
+
+class Eval(BaseModel):
+    score: int
+    nodes: int
+    moves: List[str]
+    
+    def __iadd__(self, other):
+        self.score += other.score
+        self.nodes += other.nodesss
+        self.moves += other.moves
+        return self
+    def __add__(self, other):
+        return Eval(score=self.score + other.score, nodes=self.nodes + other.nodes, moves= self.moves + other.moves)
+    def average(self):
+        return self.score / self.nodes
+
+    def __str__(self):
+        return f"Score: {self.score} | Nodes: {self.nodes} | Moves: {self.moves}"
+
+class Player(BaseModel, ABC):
+    name: str
+    @abstractmethod
+    def move(self, state):
+        pass

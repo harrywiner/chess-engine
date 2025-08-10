db0c3dfddcee75ec4c3ede7ba53efe4a196f76f3
Harry Winer <harry.winer@gmail.com>
2025-08-10 17:56:46 +0100

    Implemented multiprocessing

--- chengine/types/types.py
+++ chengine/types/types.py
@@ -19,6 +19,12 @@ class Eval(BaseModel):
     def __add__(self, other):
         # Eval + Eval
         return Eval(score=self.score + other.score, nodes=self.nodes + other.nodes, moves= self.moves + other.moves)
+    def __eq__(self, other):
+        return self.score == other.score
+    def __lt__(self, other):
+        return self.score < other.score
+    def __gt__(self, other):
+        return self.score > other.score
     def average(self):
         return self.score / self.nodes
 
